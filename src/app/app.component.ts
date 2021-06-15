004173bc89bc53c5bb448281d5098f93e65dd3b9
desaisb3 <desaisb3@vcu.edu>
2021-06-15 16:32:27 -0400

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { setAppHeight } from '@amc-technology/davinci-api';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent  implements OnInit{
   title = 'PowerApps';
+
+  ngOnInit(){
+    setAppHeight(50, false);
+  }
 }
